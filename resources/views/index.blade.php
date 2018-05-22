9a3c45550525e0eb684fb4a7e1acf6a11e777d50
CreepPork <creepporkstudios@gmail.com>
2018-05-22 15:46:02 +0300

    WIP theme & moved palette to a different URL

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.front-app')
+
+@section('content')
+
+<div class="container">
+    <h1>Hello World</h1>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/colors', function () {
     return view('color-palette');
 });
 
