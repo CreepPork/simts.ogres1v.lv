4b9125f6602577c836bdf01d57a5191b85279f00
CreepPork <creepporkstudios@gmail.com>
2018-05-23 17:46:00 +0300

    Major styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,138 @@
 @extends('layouts.app')
 
+{{-- Remove padding-top from main --}}
+@section('styling-main', 'pb-4')
+
 @section('content')
 
-<div class="container">
-    <h1>Hello World</h1>
+<div class="parallax">
+    <div class="parallax-container card">
+        <div class="card-body text-center">
+            <h4>Ogres 1. vidusskolas projekts</h4>
+            <h2>100 labie darbi</h2>
+            <div class="card-buttons">
+                <button class="ml-4 btn btn-primary">Iesaistīties</button>
+                <button class="mr-4 btn btn-outline-secondary">Uzzināt vairāk</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container container-primary">
+    <h1 class="text-center">100 labie darbi</h1>
+
+    <hr>
+
+    {{-- Tables --}}
+    <div class="row">
+
+        <div class="col-8 no-padding-right">
+            <h3 class="text-center">Paveiktie darbi</h3>
+
+            <table class="table table-bordered table-striped table-hover">
+                <thead class="table-primary">
+                    <tr>
+                        <th>Darbs</th>
+                        <th>Skolotājs</th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    <tr>
+                        <td>Darbs 1</td>
+                        <td>Skolotājs 1</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 2</td>
+                        <td>Skolotājs 2</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 3</td>
+                        <td>Skolotājs 3</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 4</td>
+                        <td>Skolotājs 4</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 5</td>
+                        <td>Skolotājs 5</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 6</td>
+                        <td>Skolotājs 6</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="col-4 no-padding-left">
+            <h3 class="text-center">Pašreizējie darbi</h3>
+
+            <table class="table table-bordered table-striped table-hover">
+                <thead class="table-primary">
+                    <tr>
+                        <th>Darbs</th>
+                        <th>Skolotājs</th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    <tr>
+                        <td>Darbs 1</td>
+                        <td>Skolotājs 1</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 2</td>
+                        <td>Skolotājs 2</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 3</td>
+                        <td>Skolotājs 3</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table class="table table-bordered table-striped table-hover">
+                <h3 class="text-center">Plānotie darbi</h3>
+
+                <thead class="table-primary">
+                    <tr>
+                        <th>Darbs</th>
+                        <th>Skolotājs</th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    <tr>
+                        <td>Darbs 1</td>
+                        <td>Skolotājs 1</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 2</td>
+                        <td>Skolotājs 2</td>
+                    </tr>
+                    <tr>
+                        <td>Darbs 3</td>
+                        <td>Skolotājs 3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    {{-- Buttons --}}
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-outline-primary">Prezentācija</button>
+        </div>
+
+        <div class="col text-right">
+            <button class="btn btn-outline-primary">Nolikums</button>
+        </div>
+    </div>
+    
+    {{-- @include('inc.lorem') --}}
 </div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <div id="app">
         @include('inc.navbar')
 
-        <main class="py-4">
+        <main class="@yield('styling-main', 'py-4')">
             @yield('content')
         </main>
     </div>
