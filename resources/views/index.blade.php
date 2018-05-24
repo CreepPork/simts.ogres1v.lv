8a0d82e764eacb7a126b9e97b295c6a275b24fd9
CreepPork <creepporkstudios@gmail.com>
2018-05-24 22:11:51 +0300

    Added events section with terrible looking calendar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
     </div>
 </div>
 
-<div class="container container-primary">
+<div class="container mb-5">
     <h1 class="text-center">100 labie darbi</h1>
 
     <hr>
@@ -133,6 +133,94 @@
     </div>
     
     {{-- @include('inc.lorem') --}}
+</div>
+
+<div class="container">
+    <h1 class="text-center">Pasākumi</h1>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-7">
+            <img src="{{ asset('images/placeholder.gif') }}" style="width: 100%" class="mb-3" alt="placeholder">
+
+            <h2>Tuvākais pasākums</h2>
+
+            <h4>Pasākums 1</h4>
+
+            <h4 class="text-muted">20. jūnijs 2018</h4>
+
+            <button class="btn btn-lg btn-primary">Skatīt</button>
+        </div>
+
+        <div class="col-5">
+            <div class="container calendar">
+                <div class="row">
+                    <h3 class="text-center">Maijs 2018</h3>
+                </div>
+
+                <div class="row">
+                    <div class="col">Pr.</div>
+                    <div class="col">Ot.</div>
+                    <div class="col">Tr.</div>
+                    <div class="col">Ce.</div>
+                    <div class="col">Pk.</div>
+                    <div class="col">Se.</div>
+                    <div class="col">Sv.</div>
+                </div>
+
+                <div class="row">
+                    <div class="col text-muted">29</div>
+                    <div class="col text-muted">30</div>
+                    <div class="col">1</div>
+                    <div class="col">2</div>
+                    <div class="col">3</div>
+                    <div class="col">4</div>
+                    <div class="col">5</div>
+                </div>
+                
+                <div class="row">
+                    <div class="col">6</div>
+                    <div class="col">7</div>
+                    <div class="col">8</div>
+                    <div class="col">9</div>
+                    <div class="col">10</div>
+                    <div class="col">11</div>
+                    <div class="col">12</div>
+                </div>
+                
+                <div class="row">
+                    <div class="col">13</div>
+                    <div class="col">14</div>
+                    <div class="col">15</div>
+                    <div class="col">16</div>
+                    <div class="col">17</div>
+                    <div class="col">18</div>
+                    <div class="col">19</div>
+                </div>
+                
+                <div class="row">
+                    <div class="col">20</div>
+                    <div class="col">21</div>
+                    <div class="col">22</div>
+                    <div class="col">23</div>
+                    <div class="col">24</div>
+                    <div class="col">25</div>
+                    <div class="col">26</div>
+                </div>
+                
+                <div class="row">
+                    <div class="col">27</div>
+                    <div class="col">28</div>
+                    <div class="col">29</div>
+                    <div class="col">30</div>
+                    <div class="col">31</div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
