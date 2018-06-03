72169c7049752c6fd7f94b6a0699b08145d6ddc9
CreepPork <creepporkstudios@gmail.com>
2018-06-03 19:58:14 +0300

    Removed npm FontAwesome and replaced with script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,5 +28,7 @@
             @yield('content')
         </main>
     </div>
+
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
 </body>
 </html>
