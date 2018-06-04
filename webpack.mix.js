1a317201c53dee3f445f752b64d8486bfc739bc5
CreepPork <creepporkstudios@gmail.com>
2018-06-04 15:29:45 +0300

    Copy changed to copy dir

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .copy('resources/assets/images/', 'public/images')
+    .copyDirectory('resources/assets/images/', 'public/images')
     .browserSync('simts.test');
