7ea674435842ea9a542836a5e15a7bf53c5541aa
CreepPork <creepporkstudios@gmail.com>
2018-06-04 15:02:45 +0300

    Renamed Home to Dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('index');
@@ -21,4 +21,4 @@ Route::get('/colors', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
