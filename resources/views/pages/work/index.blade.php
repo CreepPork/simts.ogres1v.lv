17fee5a50ed6317a374c6ca4d825194e51e28e35
CreepPork <creepporkstudios@gmail.com>
2018-06-11 17:24:03 +0300

    Added exception handling

--- resources/views/pages/work/index.blade.php
+++ resources/views/pages/work/index.blade.php
@@ -6,30 +6,36 @@
 
         <hr>
 
-        @foreach ($statuses as $status)
-            <div class="py-3">
-                <h5 class="text-center">{{ $status->status }}</h5>
-
-                <table class="table table-hover table-clickable table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th class="w-75">Nosaukums</th>
-                            <th>Skolotājs</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        @foreach ($status->works as $work)
-                            <tr>
-                                <td>{{ $work->title }}</td>
-                                <td>{{ $work->teacher->fullName() }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-
-                <hr>
+        @if (count($statuses) == 0)
+            <div class="alert alert-info">
+                Nav pievienoti darbi.
             </div>
-        @endforeach
+        @else
+            @foreach ($statuses as $status)
+                <div class="py-3">
+                    <h5 class="text-center">{{ $status->status }}</h5>
+
+                    <table class="table table-hover table-clickable table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="w-75">Nosaukums</th>
+                                <th>Skolotājs</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($status->works as $work)
+                            <tr data-href="/work/{{ $work->id }}">
+                                    <td>{{ $work->title }}</td>
+                                    <td>{{ $work->teacher->fullName() }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <hr>
+                </div>
+            @endforeach
+        @endif
     </div>
 @endsection
