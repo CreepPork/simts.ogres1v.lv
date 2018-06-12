12d46dec447b3a52aafe17406a23ee6d96d04357
CreepPork <creepporkstudios@gmail.com>
2018-06-12 19:56:57 +0300

    Added test if a work can be created #1

--- tests/Feature/WorkTest.php
+++ tests/Feature/WorkTest.php
@@ -6,6 +6,8 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Work;
+use App\WorkStatus;
 
 class WorkTest extends TestCase
 {
@@ -27,5 +29,41 @@ class WorkTest extends TestCase
         $response->assertSee('Pabeigtie darbi');
         $response->assertSee('Plānotie darbi');
         $response->assertSee('Pašreizējie darbi');
+    }
+
+    /** @test */
+    public function a_work_can_be_created()
+    {
+        $response = $this->post('/work', [
+            'title' => 'A title.',
+            'body' => 'A body.',
+
+            'completion' => '',
+
+            'teacher_id' => $this->create('App\Teacher')->id,
+            'work_status_id' => WorkStatus::where('status', '=', 'Pabeigtie darbi')->get()->first()->id
+        ]);
+
+        $this->assertDatabaseHas('works', [
+            'title' => 'A title.'
+        ]);
+
+        $work = Work::latest()->first();
+
+        $this->get('/')->assertSee('A title.');
+        $this->get('/work')->assertSee('A title.');
+        $this->get('/work/' . $work->id)->assertSee('A title.');
+
+        $this->get('/')
+            ->assertSee($work->title)
+            ->assertSee($work->teacher->fullName());
+
+        $this->get('/work')
+            ->assertSee($work->title)
+            ->assertSee($work->teacher->fullName());
+
+        $this->get('/work/' . $work->id)
+            ->assertSee($work->title)
+            ->assertSee($work->teacher->fullName());
     }
 }
