b4a9aeb928313f5db091587a57358968ac06c45e
CreepPork <creepporkstudios@gmail.com>
2018-06-12 18:37:09 +0300

    Set foreign key constraint enforcement to on

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,19 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Tests\Utilities\FactoryHelpers;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use FactoryHelpers;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        DB::statement('PRAGMA foreign_keys=on;');
+    }
 
     protected function signIn($user = null)
     {
