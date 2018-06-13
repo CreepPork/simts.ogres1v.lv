3522993a80b4cf8d827b772ab0c18448b1d5ab1f
CreepPork <creepporkstudios@gmail.com>
2018-06-13 14:24:51 +0300

    Added auth middleware to Work controller #8

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -8,6 +8,11 @@ use App\WorkStatus;
 
 class WorkController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
