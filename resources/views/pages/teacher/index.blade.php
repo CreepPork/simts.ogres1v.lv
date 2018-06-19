9ce21839259e039291ce4d9f74e2b5dcac53e721
Ralfs <creepporkstudios@gmail.com>
2018-06-19 17:01:10 +0300

    Added index layout for teachers

---
+++ resources/views/pages/teacher/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('navbar-links')
+    <li>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/dashboard">Informācijas panelis</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Skolotāji</li>
+        </ol>
+    </li>
+@endsection
+
+@section('content')
+    <div class="wrapper container">
+        <h1 class="text-center">Skolotāji</h1>
+
+        <hr>
+
+        @include('inc.messages')
+
+        <table class="table table-hover table-clickable table-striped">
+            <thead>
+                <tr>
+                    <th>Skolotājs</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($teachers as $teacher)
+                    <tr data-href="/teacher/{{ $teacher->id }}">
+                        <td>{{ $teacher->fullName() }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
