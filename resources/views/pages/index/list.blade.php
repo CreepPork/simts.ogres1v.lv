7a3868f9dd93b4f921854edd3c4e4d64c3fb5432
Ralfs <creepporkstudios@gmail.com>
2018-06-20 14:05:48 +0300

    Added list layout #22

---
+++ resources/views/pages/index/list.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('navbar-links')
+    <li>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/dashboard">Informācijas panelis</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Rediģēt lapas saturu</li>
+        </ol>
+    </li>
+@endsection
+
+@section('content')
+    <div class="wrapper container">
+        <h1 class="text-center">Rediģēt lapas saturu</h1>
+
+        <hr>
+
+        <table class="table table-clickable table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>Sekcija</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($indexes as $index)
+                    <tr data-href="/index/{{ $index->id }}">
+                        <td>{{ $index->section_title }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
