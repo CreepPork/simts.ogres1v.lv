5a36bb1eebcaca9c50bfdb9ad273fc02bc8796c3
Ralfs <creepporkstudios@gmail.com>
2018-06-20 17:52:56 +0300

    Added index tests #22 #1

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -25,4 +25,45 @@ class IndexTest extends TestCase
 
         $response->assertSee('100 labie darbi');
     }
+
+    /** @test */
+    public function all_pages_except_main_index_page_require_auth()
+    {
+        $this->get('/')->assertStatus(200);
+
+        $this->get('/index')->assertRedirect('/login');
+        $this->get('/index/1/edit')->assertRedirect('/login');
+        $this->patch('/index/1')->assertRedirect('/login');
+        $this->delete('/index/1')->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function all_indexes_are_listed()
+    {
+        $this->signIn();
+
+        $index = $this->create('App\Index');
+
+        $this->get('/index')->assertSee($index->section_title);
+    }
+
+    /** @test */
+    public function indexes_can_be_edited()
+    {
+        $this->signIn();
+
+        $index = $this->create('App\Index');
+
+        $this->get('/index/' . $index->id . '/edit')
+            ->assertSee($index->section_title);
+
+        $this->patch('/index/' . $index->id, [
+            'section' => $index->section,
+            'section_title' => 'New Section Title',
+            'title' => $index->title,
+            'body' => $index->body,
+        ])->assertSessionHas('success');
+
+        $this->get('/index')->assertSee('New Section Title');
+    }
 }
