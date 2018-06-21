3acb04ca384bb1f9b1cdde971585d06087951d10
Ralfs <creepporkstudios@gmail.com>
2018-06-21 15:38:04 +0300

    Added gift.js with logic for destroy butotn and textarea helper #9

---
+++ resources/assets/js/pages/gift.js
@@ -0,0 +1,9 @@
+import { textareaHeight } from '../helpers/textareaHeight';
+
+textareaHeight();
+
+$('#destroyButton').click(function () {
+    let id = this.dataset.id;
+
+    axios.delete(`/gift/${id}`).then(() => window.location.reload(true));
+});
