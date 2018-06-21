fc4eb350a3bd00775d1158b5384cb6d50f73ced2
Ralfs <creepporkstudios@gmail.com>
2018-06-21 15:00:33 +0300

    Added dashboard navbar links and gift page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
+@section('navbar-links')
+    <li>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active" aria-current="page">Informācijas panelis</li>
+        </ol>
+    </li>
+@endsection
+
 @section('content')
     <div class="container wrapper">
         <h1 class="text-center">Informācijas panelis</h1>
@@ -10,6 +18,7 @@
 
         <div class="btn-group d-flex justify-content-center">
             <a href="/recommend" class="btn btn-primary">Ieteikumi</a>
+            <a href="/gift" class="btn btn-primary">Dāvināt</a>
             <a href="/work" class="btn btn-primary">Darbi</a>
             <a href="/workStatus" class="btn btn-primary">Darba statusi</a>
             <a href="/teacher" class="btn btn-primary">Skolotāji</a>
