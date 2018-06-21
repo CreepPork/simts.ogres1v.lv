a03af0bb8b31a0abe68caa259ab1df65ec368fa1
Ralfs <creepporkstudios@gmail.com>
2018-06-21 15:16:02 +0300

    Added layout for create gift #9

---
+++ resources/views/pages/gift/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('navbar-links')
+    <li>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/">Sākums</a></li>
+            <li class="breadcrumb-item"><a href="/gift">Dāvināt</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Pievienot aprakstu</li>
+        </ol>
+    </li>
+@endsection
+
+@section('content')
+    <div class="wrapper container">
+        <h1 class="text-center">Pievienot aprakstu</h1>
+
+        <hr>
+
+        @include('inc.messages')
+
+        <form action="/gift" method="post">
+            @csrf
+
+            <div class="form-group required">
+                <label for="title">Nosaukums</label>
+                <input type="text" name="title" id="title" class="form-control" placeholder="Nosaukums" required autofocus>
+            </div>
+
+            <div class="form-group required">
+                <label for="body">Apraksts</label>
+                <textarea name="body" id="body" cols="30" rows="10" class="form-control" placeholder="Apraksts" required></textarea>
+            </div>
+
+            @include('inc.required')
+
+            <div class="form-group">
+                <input type="submit" value="Pievienot" class="btn btn-outline-primary form-control">
+            </div>
+        </form>
+    </div>
+@endsection
