dd3019ed4b605ce85a00d32a21e051c6e46fdfc9
Ralfs <creepporkstudios@gmail.com>
2018-06-25 21:24:38 +0300

    Added Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
         </main>
     </div>
 
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+    @include('inc.ga')
     @yield('scripts')
 </body>
 </html>
