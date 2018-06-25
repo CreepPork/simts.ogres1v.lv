5a17cf7574902d7e5653a6bcc253e804f3854c6c
Ralfs <creepporkstudios@gmail.com>
2018-06-25 21:25:52 +0300

    Performance improvements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,6 @@
 
     <title>100 labie darbi - {{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
@@ -31,6 +28,8 @@
         </main>
     </div>
 
+    <!-- Scripts -->
+    <script defer src="{{ asset('js/app.js') }}"></script>
     <script async defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
     @include('inc.ga')
     @yield('scripts')
