09edab3370e105e602c11c0c87e4bfe856328bf6
Ralfs <creepporkstudios@gmail.com>
2018-06-29 14:54:10 +0300

    Sorted dashboard by alphabetical order

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,13 +17,13 @@
         @include('inc.messages')
 
         <div class="btn-group d-flex justify-content-center">
+            <a href="/workStatus" class="btn btn-primary">Darba statusi</a>
+            <a href="/work" class="btn btn-primary">Darbi</a>
+            <a href="/gift" class="btn btn-primary">Dāvināt</a>
             <a href="/recommend" class="btn btn-primary">Ieteikumi</a>
             <a href="/event" class="btn btn-primary">Pasākumi</a>
-            <a href="/gift" class="btn btn-primary">Dāvināt</a>
-            <a href="/work" class="btn btn-primary">Darbi</a>
-            <a href="/workStatus" class="btn btn-primary">Darba statusi</a>
-            <a href="/teacher" class="btn btn-primary">Skolotāji</a>
             <a href="/index" class="btn btn-primary">Rediģēt galvenās lapas saturu</a>
+            <a href="/teacher" class="btn btn-primary">Skolotāji</a>
         </div>
     </div>
 @endsection
