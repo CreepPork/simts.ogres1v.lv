e7afe2a7ca4b3463b0ca6cefcd642a62b48442a5
Ralfs <creepporkstudios@gmail.com>
2018-06-29 15:49:49 +0300

    Fixed broken pageID getter

--- resources/assets/js/pages/indexAdmin.js
+++ resources/assets/js/pages/indexAdmin.js
@@ -2,15 +2,15 @@ import { textareaHeight } from '../helpers/textareaHeight';
 
 textareaHeight();
 
-let pageID = this.href.substr().split('/')[4];
+let pageID = document.location.href.substr().split('/')[4];
 
-$('#imageDestroyButton').click(function (e) {
+$('#imageDestroyButton').click(e => {
     e.preventDefault();
 
     axios.delete(`/index/${pageID}/image`).then(() => window.location.reload(true));
 });
 
-$('#fileDestroyButton').click(function (e) {
+$('#fileDestroyButton').click(e => {
     e.preventDefault();
 
     axios.delete(`/index/${pageID}/file`).then(() => window.location.reload(true));
