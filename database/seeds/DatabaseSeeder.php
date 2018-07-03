6f432498b543dd5d559bfc58ca64ba446fbcd36c
Ralfs <creepporkstudios@gmail.com>
2018-07-03 16:04:02 +0300

    Added database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Event;
+use App\Gift;
+use App\Recommendation;
+use App\Work;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Have to run app:setup to make work factories work.
+        Artisan::call('app:setup');
+
+        // Make a user (tester) so we don't have to run an additional command.
+        Artisan::call('make:user');
+
+        // Event
+        factory(Event::class, 8)->create();
+
+        // Gift
+        factory(Gift::class, 5)->create();
+
+        // Recommendation
+        factory(Recommendation::class, 3)->create();
+
+        // Works
+        // Planned works
+        factory(Work::class, 20)->create(['work_status_id' => 1]);
+
+        // Current works
+        factory(Work::class, 20)->create(['work_status_id' => 2]);
+
+        // Completed works
+        factory(Work::class, 20)->create(['work_status_id' => 3]);
     }
 }
