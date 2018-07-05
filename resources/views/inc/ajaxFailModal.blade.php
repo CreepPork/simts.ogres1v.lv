8370bb4dab503854a7698bde5bb0198e237fe369
Ralfs <creepporkstudios@gmail.com>
2018-07-05 16:41:03 +0300

    Added fail modal for Axios #38

---
+++ resources/views/inc/ajaxFailModal.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="failModal" tabindex="-1" role="dialog" aria-labelledby="failModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="failModalLabel">Neizdevās veikt darbību!</h5>
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                Neizdevās veikt jūsu izvelēto darbību! Mēģiniet vēlreiz vēlāk!
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal" aria-label="Close">Sapratu</button>
+            </div>
+        </div>
+    </div>
+</div>
