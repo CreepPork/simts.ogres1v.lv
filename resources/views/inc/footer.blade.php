ffa21a982fbf1f4dcd7c1cbb01336b61c4afc858
Ralfs <creepporkstudios@gmail.com>
2019-01-24 21:44:53 +0200

    Updated copyright year, fixed #54

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row align-items-center justify-content-between">
             <div class="col-sm-6 col-md-5 col-copyright">
-                <a class="school-link" href="http://ogres1v.lv/">Ogres 1. vidusskola &copy; 2018</a>
+                <a class="school-link" href="http://ogres1v.lv/">Ogres 1. vidusskola &copy; 2019</a>
             </div>
 
             <div class="col-sm-4 col-md-3 justify-content-between footer-icons">
